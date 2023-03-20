a3989df607423bc737ea8f849ab129663fe69999
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-20 09:13:22 -0300

    Ex3 - updating data from the db

---
+++ src/application/Ex3.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+import db.DbException;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		PreparedStatement st = null; 
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("UPDATE seller "  //SEMPRE USE UPDATE COM WHERE, SEMPRE
+									 + "SET BaseSalary = BaseSalary + ? "
+									 + "WHERE "
+									 + "(DepartmentId = ?)");
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());		
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
