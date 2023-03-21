b8643e9d1392b0b9d5470b6f6999798ded7e0d54
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-21 15:34:52 -0300

    DAO Project - Updating data from the db

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,9 +30,16 @@ public class Program {
 		System.out.println();
 		
 		System.out.println("---Test 4: Seller insert---");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(null, "Bruce", "batman@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New ID = " + newSeller.getId());
+		System.out.println();
+		
+		System.out.println("---Test 5: Seller update---");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 		System.out.println();
 	}
 
