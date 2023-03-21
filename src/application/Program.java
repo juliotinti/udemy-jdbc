8e45cc9d8d93e2454c8c5f34105b011efcb67caa
Julio Tinti <julio_tinti_andrade@hotmail.com>
2023-03-21 15:21:04 -0300

    DAO Project - Implementing findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,11 @@ public class Program {
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 		System.out.println();
+		
+		System.out.println("---Test 3: Seller findAll---");
+		list = sellerDao.findAll();
+		list.forEach(System.out::println);
+		System.out.println();
 	}
 
 }
